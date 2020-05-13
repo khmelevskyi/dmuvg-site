11a7886d91181a08f8860e1a9a40fddf8818310d
Yevhen Khmelevskyi <khmellevskyi.ye23@gmail.com>
2020-05-13 18:30:39 +0300

    first blocks

---
+++ src/pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home_view(request, *args, **kwargs): #request , #request.user
+    #return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
+
+
+def contact_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Contact page</h1>")
+
